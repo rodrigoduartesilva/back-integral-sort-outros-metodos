ff50a99ffb58490b0adf0cd10ebacfd45d2cb89d
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-01-18 18:31:11 -0300

    Classe 02

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,1 +1,7 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+const maiorString = cidades.reduce(function (acumulador, atual) {
+    return atual.length > acumulador.length ? atual : acumulador;
+});
+
+console.log(maiorString);
