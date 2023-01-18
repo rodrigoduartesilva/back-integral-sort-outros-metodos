44514d92b23970c38938db67e4e439b530175ad3
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-01-18 19:30:54 -0300

    Classe 05 - Finalizado

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,1 +1,6 @@
 const palavras = ["arroz", "feijão", "carne", "cerveja", "macarrão"];
+
+const procurarPalavras = palavras.some(x => x === 'cerveja' || x === 'vodka');
+
+
+procurarPalavras ? console.log('Revise sua lista, João. Possui bebida com venda proibida!') : console.log('Tudo certo, vamos as compras!');
