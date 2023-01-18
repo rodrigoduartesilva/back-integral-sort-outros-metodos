8c2cd6c41c2372d555f4ed68634bc1b3e3101240
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-01-18 18:52:29 -0300

    Classe 04 - Finalizado

--- classe-04/index.js
+++ classe-04/index.js
@@ -1,1 +1,10 @@
 const palavras = ['livro', 'caneta', 'sol', 'carro', 'orelha'];
+
+const maiorNumero = palavras.some(x => x.length > 5);
+
+
+if (maiorNumero) {
+    console.log('Existe palavra inválida');
+} else {
+    console.log('Array validado');
+}
